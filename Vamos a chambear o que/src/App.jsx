88623d6d1904e467c1ad3d27b6cb2c58d3522f7a
track1at01 <ricardoawnil@gmail.com>
2026-05-09 10:56:49 -0500

    xd

--- Vamos a chambear o que/src/App.jsx
+++ Vamos a chambear o que/src/App.jsx
@@ -29,34 +29,12 @@ function App() {
 
 
 <div className='productos'>
-
-      <Producto
-        {...PRODUCTOS[0]}
+      {PRODUCTOS.map((producto, index) => (
+        <Producto
+          key={index}
+          {...producto}
         />
-      <Producto
-        {...PRODUCTOS[1]}
-        />
-      <Producto
-        {...PRODUCTOS[2]}
-        />
-      <Producto
-        {...PRODUCTOS[3]}
-        />
-      <Producto
-        {...PRODUCTOS[4]}
-        />
-      <Producto
-        {...PRODUCTOS[5]}
-        />
-      <Producto
-        {...PRODUCTOS[6]}
-        />
-      <Producto
-        {...PRODUCTOS[7]}
-        />
-      <Producto
-        {...PRODUCTOS[8]}
-        />
+      ))}
         </div>
     </main>
   )
