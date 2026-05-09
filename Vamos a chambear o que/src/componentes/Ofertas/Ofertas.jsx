76c33cbdd71215b849e245f6357f40f6b03e2ef0
track1at01 <ricardoawnil@gmail.com>
2026-05-09 08:25:32 -0500

    tetas

---
+++ Vamos a chambear o que/src/componentes/Ofertas/Ofertas.jsx
@@ -0,0 +1,12 @@
+export default function Oferta(imagePath, descuento, nombreProducto){
+    return(
+<div>
+<div className="divDescuento">
+<h2></h2>
+</div>
+
+    <h2>nombreProducto</h2>
+</div>
+
+    )
+}
